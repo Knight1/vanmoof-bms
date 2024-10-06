378e792f693909cda4bc369f97284a472bafc787
Tobias <sachstobia@aol.com>
2024-10-06 05:46:10 +0200

    Fix for the go test Error

--- livedata.go
+++ livedata.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"time"
 
 	"github.com/simonvetter/modbus"
 )
 
 // very fragile!
+// TODO: GRAB Screen and update only that part
 func liveData(client *modbus.ModbusClient, debug bool) {
 
 	for {
 		// 0x0000 is start otherwise the case statement will not work because 2 gets 0 there.
 		if regs, err = readRegisters(client, 0, 44); err != nil {
-			fmt.Println("Failed to read registers: %v", err)
+			log.Printf("Failed to read registers: %v", err)
 			if err = connectToBMS(client, debug); err != nil {
-				fmt.Println("Failed to connect to BMS: %v", err)
+				log.Printf("Failed to connect to BMS: %v", err)
 			}
 		}
 		getAndShowPassiveBMSData()
