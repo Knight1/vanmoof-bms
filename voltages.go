7f996710e9656f5bb28e5a17b7f9ac067508b1a4
Tobias <sachstobia@aol.com>
2024-12-24 22:21:26 +0100

    Improved Cell imbalance logic.

--- voltages.go
+++ voltages.go
@@ -5,15 +5,18 @@ import (
 	"math"
 )
 
+// passive means live
 func getAndShowPassiveVoltages() {
 	fmt.Println("-- BEGIN LIVE VOLTAGES --")
+
+	cellImbalanced := false
 
 	// Voltage Cell Pack Monitoring
 	for register, value := range regs {
 		milliVolts = float64(value) / 1000
 		switch register {
 		case 4:
-			fmt.Println("Pack:", milliVolts, "Volts")
+			fmt.Println("Pack:  ", milliVolts, "Volts")
 		case 27:
 			fmt.Println("Cell 1:", milliVolts, "Volts")
 		case 28:
@@ -33,7 +36,7 @@ func getAndShowPassiveVoltages() {
 		case 35:
 			fmt.Println("Cell 9:", milliVolts, "Volts")
 		case 36:
-			fmt.Println("Cell 10:", milliVolts, "Volts")
+			fmt.Println("Cell X:", milliVolts, "Volts")
 
 			// TODO: Check if one cell is below these values
 
@@ -42,14 +45,14 @@ func getAndShowPassiveVoltages() {
 			continue
 		}
 
-		// Verify if Voltage is to low, to high
+		// Verify if Voltage is too low, too high
 		if register != 4 && value < cellVoltageLow {
 			fmt.Println("Voltage in Cell to LOW!", register)
 		} else if register != 4 && value > cellVoltageHigh {
 			fmt.Println("Voltage in Cell to HIGH!", register)
 		}
 
-		// TODO: Define Pack Voltage Limits from BMS!
+		// Check if Pack Voltage is too low or too high
 		if register == 4 && value < packVoltageLow {
 			fmt.Println("Voltage in Pack to LOW!")
 		} else if register == 4 && value > packVoltageHigh {
@@ -57,15 +60,25 @@ func getAndShowPassiveVoltages() {
 		}
 
 		// TODO: Check Voltage imbalance trigger
-		// Check for Voltage Imbalances in the Cells
-		diff := math.Abs(float64(cellVoltagePrevious - value))
+		// Check for Voltage Imbalances in the Cells from live Values by own means.
+		diff := math.Abs(float64(int(cellVoltagePrevious) - int(value)))
 		if diff > float64(cellVoltageImbalance) {
-			//fmt.Println("Voltage Imbalance in Cells!")
-			fmt.Println("DEBUG: Value ", value, "differs from previous value by more than", diff)
-		}
-		cellVoltagePrevious = value
-		//fmt.Println("DEBUG: Value", value, "differs from previous value by", diff)
+			// Ignore Pack Voltage and the first Cell. Because there is nothing to compare it to.
+			if register != 4 && register != 27 {
+				cellImbalanced = true
+				if debug {
+					fmt.Println("DEBUG: Value ", value, "differs from previous value", cellVoltagePrevious, "by more than", diff)
+				}
 
+			}
+		}
+		if register != 4 && value != 0 {
+			cellVoltagePrevious = value
+		}
+	}
+
+	if cellImbalanced == true {
+		fmt.Println("WARNING: Voltage Imbalance in Cells!")
 	}
 
 	fmt.Println("-- END LIVE VOLTAGES --")
