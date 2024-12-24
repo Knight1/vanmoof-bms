bf0f16662f96dcf2e63bd6ed37af05ad589bb497
Tobias <sachstobia@aol.com>
2024-12-24 22:48:55 +0100

    Check Fault Status in Flash Data. Check all Records for plausibility.

--- flashdata.go
+++ flashdata.go
@@ -10,43 +10,98 @@ func getAndShowFlashBMSData() {
 		switch register {
 		case 48:
 			// TODO: Check what this is, should be a hex output
-			fmt.Println("Fault Status:", value)
+
+			if value != 0 {
+				fmt.Printf("Register 0x%X ('Fault Status'): 0x%04X\n", 0x2, value)
+
+				// Decode flags (bitwise operations)
+				flags := []string{"DOTP", "DUTP", "COTP", "CUTP", "DOCP1", "DOCP2", "COCP1", "COCP2", "OVP1", "OVP2", "UVP1", "UVP2", "PDOCP", "PDSCP", "MOTP", "SCP"}
+				for i, flag := range flags {
+					if value&(1<<i) != 0 {
+						fmt.Printf(" - %s is set\n", flag)
+					}
+				}
+			}
 		case 49:
-			// TODO: Reports records that are too high
-			fmt.Println("Battery Temperature Sensor 1 Record:", calculateCelsius(value), "°C")
+			// TODO: Reports records that are too high (65535)
+			if value < 3731 && value > 2431 {
+				fmt.Println("Battery Temperature Sensor 1 Record:", calculateCelsius(value), "°C")
+			} else {
+				fmt.Println("Battery Temperature Sensor 1 Record to high or to low!")
+			}
+
 		case 50:
-			// TODO: Reports records that are too high
-			fmt.Println("Battery Temperature Sensor 2 Record:", calculateCelsius(value), "°C")
+			if value < 3731 && value > 2431 {
+				fmt.Println("Battery Temperature Sensor 2 Record:", calculateCelsius(value), "°C")
+			} else {
+				fmt.Println("Battery Temperature Sensor 2 Record to high or to low!")
+			}
 		case 51:
-			// TODO: Reports records that are too high
-			fmt.Println("MOSFET Temperature Record:", calculateCelsius(value), "°C")
+			if value > 0 {
+				fmt.Println("MOSFET Temperature Record:", calculateCelsius(value), "°C")
+			} else {
+				fmt.Println("MOSFET Temperature Record is set to 0!")
+			}
 		case 52:
-			fmt.Println("Battery Voltage Record:", value, "mV")
+			if value < 4000 {
+				fmt.Println("Battery Voltage Record is below 4 Volts!")
+			} else {
+				fmt.Println("Battery Voltage Record:", value, "mV")
+			}
+
 		case 53:
 			fmt.Println("Current: ", calculateAmperes(value), "mA")
 		case 54:
-			fmt.Println("Full Charge Capacity:", value, "%")
+			if value < 10000 {
+				fmt.Println("Full Charge Capacity below 10000 mAh!")
+			}
+			// Does not make sense
+			fmt.Println("Full Charge Capacity:", value, "mAh")
 		case 55:
-			fmt.Println("Remaining Capacity:", value, "%")
+			// Does not make sense
+			fmt.Println("Remaining Capacity:", value, "mAh")
 		case 56:
-			// TODO: shows 0 insted of Real State of Charge
+			fmt.Println("RSOC: ", value, "%")
+			// TODO: shows 0 or something to high instead of Real State of Charge
 		case 57:
-			// TODO: shows 0 insted of Absolute State of Charge
+			fmt.Println("Absolute SOC", value, "%")
+			// TODO: shows 0 or something to high instead of Absolute State of Charge
 		case 58:
-			// TODO: shows 0 instead of Cycle Count
+			if value != 0 {
+				fmt.Println("Cycle Count:", value)
+			} else {
+				fmt.Println("No Cycle Count set in Flash. Check Passive Data Values!")
+			}
+
+			if value != regs[19] {
+				fmt.Println("Flash Data and Passive Cycle Count mismatch! Real Cycle Count:", regs[19])
+			}
+
 		case 59:
 			// register 59 to 68 are Cell Voltages from flash
 			// Values are either 0 or something odd
 		case 87:
-			fmt.Println("Maximal recorded Charging Current:", calculateAmperes(value), "mA")
+			fmt.Println("Maximum recorded Charging Current:", calculateAmperes(value), "mA")
 		case 88:
-			fmt.Println("Maximal recorded Discharging Current:", calculateAmperes(value), "mA")
+			fmt.Println("Maximum recorded Discharging Current:", calculateAmperes(value), "mA")
 		case 89:
-			fmt.Println("Maximal recorded Cell Temperature:", calculateCelsius(value), "°C")
+			if value < 3731 && value > 2431 {
+				fmt.Println("Maximum recorded Cell Temperature:", calculateCelsius(value), "°C")
+			} else {
+				fmt.Println("Maximum recorded Cell Temperature is set to high!")
+			}
 		case 90:
-			fmt.Println("Minimal recorded Cell Temperature:", calculateCelsius(value), "°C")
+			if value < 3731 && value > 2431 {
+				fmt.Println("Minimal recorded Cell Temperature:", calculateCelsius(value), "°C")
+			} else {
+				fmt.Println("Minimal recorded Cell Temperature is set to high!")
+			}
 		case 91:
-			fmt.Println("Maximal recorded MOSFET Temperature:", calculateCelsius(value), "°C")
+			if value < 3731 && value > 2431 {
+				fmt.Println("Maximum recorded MOSFET Temperature:", calculateCelsius(value), "°C")
+			} else {
+				fmt.Println("Maximum recorded MOSFET Temperature is set to high!")
+			}
 		default:
 			continue
 		}
