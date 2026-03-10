122ae1ec1689da542248df944f035ce4cebf4c55
Tobias <sachstobia@aol.com>
2026-03-10 23:36:08 +0100

    spelling mistake on TurnDischargeOn also I wrote 0 into the register so the Discharge was never turn on :(

--- internal/actions.go
+++ internal/actions.go
@@ -99,10 +99,10 @@ func TurnDebugOff(client *modbus.ModbusClient) {
 }
 
 func TurnDischargingOn(client *modbus.ModbusClient) {
-	if err := client.WriteRegister(0x8, 0); err != nil {
-		fmt.Println("Error setting Discharging to Off. Error:", err)
+	if err := client.WriteRegister(0x8, 1); err != nil {
+		fmt.Println("Error setting Discharging to On. Error:", err)
 	} else {
-		fmt.Println("Discharging set to Off!")
+		fmt.Println("Discharging set to On!")
 	}
 }
 
