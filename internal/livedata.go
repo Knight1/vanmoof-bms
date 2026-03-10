1b02bb8abaaf662abd84e7225475d7b803bd98ce
Tobias <sachstobia@aol.com>
2026-03-11 00:31:05 +0100

    LiveData now also shows the Bootloader Version

--- internal/livedata.go
+++ internal/livedata.go
@@ -13,7 +13,7 @@ func LiveData(client *modbus.ModbusClient, debug bool) {
 
 	for {
 		// 0x0000 is start otherwise the case statement will not work because 2 gets 0 there.
-		if _, readErr := ReadRegisters(client, 0, 44); readErr != nil {
+		if _, readErr := ReadRegisters(client, 0, 45); readErr != nil {
 			log.Printf("Failed to read registers: %v", readErr)
 			if _, err := ConnectToBMS(client, debug); err != nil {
 				log.Printf("Failed to connect to BMS: %v", err)
